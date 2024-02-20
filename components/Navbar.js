093d088638f24bd004803c58267d7c7bb93a57df
Teerth Patel <pteerth989@gmail.com>
2024-02-20 21:40:01 +0530

    map added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import avatar from '../temp/avatar.jpg'
-import { BsPerson } from 'react-icons/bs'
-import { useContext } from 'react'
-import { UberContext } from '../context/uberContext'
+import Image from "next/image";
+import avatar from "../temp/avatar.jpg";
+import { BsPerson } from "react-icons/bs";
+import { useContext } from "react";
+import { UberContext } from "../context/uberContext";
 
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
@@ -14,10 +14,11 @@ const style = {
   userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
   loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
   loginText: `ml-2`,
-}
+};
 
 const Navbar = () => {
-  const { currentAccount, connectWallet, currentUser } = useContext(UberContext)
+  const { currentAccount, connectWallet, currentUser } =
+    useContext(UberContext);
 
   return (
     <div className={style.wrapper}>
@@ -28,20 +29,34 @@ const Navbar = () => {
         <div className={style.menuItem}>More</div>
       </div>
       <div className={style.rightMenu}>
-        <div className={style.menuItem}>Help</div>
-        <div className={style.menuItem}>{currentUser.name?.split(' ')[0]}</div>
-        <div className={style.userImageContainer}>
-          <Image
-            className={style.userImage}
-            src={avatar}
-            width={40}
-            height={40}
-          />
-        </div>
-        {currentAccount ? (
-          <div>
-            {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
-          </div>
+        {currentUser ? (
+          <>
+            <div className={style.menuItem}>Help</div>
+            <div className={style.menuItem}>
+              {currentUser.name?.split(" ")[0]}
+            </div>
+            <div className={style.userImageContainer}>
+              <Image
+                className={style.userImage}
+                src={avatar}
+                width={40}
+                height={40}
+              />
+            </div>
+            {currentAccount ? (
+              <div>
+                {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
+              </div>
+            ) : (
+              <div
+                className={style.loginButton}
+                onClick={() => connectWallet()}
+              >
+                <BsPerson />
+                <span className={style.loginText}>Log in</span>
+              </div>
+            )}
+          </>
         ) : (
           <div className={style.loginButton} onClick={() => connectWallet()}>
             <BsPerson />
@@ -50,7 +65,7 @@ const Navbar = () => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
   },
-}
+  env: {
+    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
+      process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
